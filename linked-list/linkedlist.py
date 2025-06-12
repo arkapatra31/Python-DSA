97be1e17083e4c62bd90cee554db0738144041bb
Arka <arkapatra@deloitte.com>
2025-06-12 23:21:10 +0530

    Get | Set Node value by Index
    Signed-off-by: Arka <arkapatra@deloitte.com>

--- linked-list/linkedlist.py
+++ linked-list/linkedlist.py
@@ -86,8 +86,25 @@ class LinkedList:
             self.head = self.head.next
             self.length -= 1
             return deleted_node
-        
-        
+
+    def get_node_by_index(self, index: int):
+        """Returns the node at the given index."""
+        if index < 0 or index >= self.length:
+            return None
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current
+
+    def set_node_value_by_index(self, index: int, value):
+        """Sets the value of the node at the given index."""
+        if index < 0 or index >= self.length:
+            return None
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        current.value = value
+        return self
 
 # Clear console
 os.system("clear")
@@ -95,9 +112,13 @@ my_linked_list = LinkedList(4)
 print(my_linked_list.head.value)  # Output: 4
 my_linked_list.append(5)
 print(my_linked_list.tail.value)  # Output: 5
+my_linked_list.print_list()
+print(f"Node at Index 1: {my_linked_list.get_node_by_index(1).value}")
 print(my_linked_list.delete_last_node().value)  # Output: 5
 my_linked_list.print_list()
 my_linked_list.prepend(6)
 my_linked_list.print_list()
 print(my_linked_list.pop_first().value)  # Output: 6
+my_linked_list.print_list()
+my_linked_list.set_node_value_by_index(1, 10)
 my_linked_list.print_list()
